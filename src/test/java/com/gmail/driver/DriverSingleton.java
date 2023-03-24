23aef56a33d4f3009cccec319b46b048f9880ce6
m-starilov <starilov.m@gmail.com>
2023-03-24 13:50:45 +0600

    firefox webdriver deleted

--- src/test/java/com/gmail/driver/DriverSingleton.java
+++ src/test/java/com/gmail/driver/DriverSingleton.java
@@ -4,7 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
 
@@ -15,16 +14,11 @@ public class DriverSingleton {
 
     public static WebDriver getDriver() {
         if (null == driver) {
-            if ("firefox".equals(System.getProperty("browser"))) {
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-            } else {
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--start-maximized");
-                chromeOptions.addArguments("--remote-allow-origins=*");
-                driver = new ChromeDriver(chromeOptions);
-            }
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--start-maximized");
+            chromeOptions.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver(chromeOptions);
         }
         return driver;
     }
